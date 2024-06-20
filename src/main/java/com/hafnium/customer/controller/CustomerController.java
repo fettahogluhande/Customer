28d2bb44c3945626a4dd20e8315d45bd8c381945
Hande Fettahoğlu <fettahogluhande@gmail.com>
2024-06-20 11:18:20 +0300

    add getAll & getFindById method

--- src/main/java/com/hafnium/customer/controller/CustomerController.java
+++ src/main/java/com/hafnium/customer/controller/CustomerController.java
@@ -3,19 +3,25 @@ package com.hafnium.customer.controller;
 import com.hafnium.customer.model.Customer;
 import com.hafnium.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
-
     @PostMapping()
     public Customer addCustomer(@RequestBody Customer customer){
         return customerService.saveCustomer(customer);
+    }
+    @GetMapping()
+    public List<Customer> allCustomer(){
+        return customerService.allCustomer();
+    }
+    @GetMapping("{id}")
+    public Customer getCustomerById(@PathVariable("id") Long id){
+        return customerService.getCustomerFindById(id);
     }
 }
--- src/main/java/com/hafnium/customer/service/CustomerService.java
+++ src/main/java/com/hafnium/customer/service/CustomerService.java
@@ -3,13 +3,31 @@ package com.hafnium.customer.service;
 import com.hafnium.customer.model.Customer;
 import com.hafnium.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerService {
     private final CustomerRepository customerRepository;
     public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
+    }
+    public List<Customer> allCustomer() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerFindById(Long id) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        if (optionalCustomer.isPresent()) {
+            return optionalCustomer.get();
+        }else {
+            log.info("Customer not found");
+            return null;
+        }
     }
 }
